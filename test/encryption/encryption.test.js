69ba0708a6b4faabca9de2d2bfebb5cf9863343e
Filip Skokan <panva.ip@gmail.com>
2016-08-04 18:15:52 +0200

    fix: unsigned encrypted userinfo responses are not signed anymore

--- test/encryption/encryption.test.js
+++ test/encryption/encryption.test.js
@@ -23,34 +23,36 @@ before(function () {
   describe(`[encryption] IMPLICIT id_token+token ${verb} ${route}`, function () {
     before(agent.login);
 
-    before(function () {
-      const auth = new AuthorizationRequest({
-        response_type: 'id_token token',
-        scope: 'openid'
+
+    describe('encrypted authorization results', function () {
+      before(function () {
+        const auth = new AuthorizationRequest({
+          response_type: 'id_token token',
+          scope: 'openid'
+        });
+
+        return wrap({ agent, route, verb, auth })
+        .expect(auth.validateFragment)
+        .expect((response) => {
+          const { query } = url.parse(response.headers.location, true);
+          this.id_token = query.id_token;
+          this.access_token = query.access_token;
+        });
       });
 
-      return wrap({ agent, route, verb, auth })
-      .expect(auth.validateFragment)
-      .expect((response) => {
-        const { query } = url.parse(response.headers.location, true);
-        this.id_token = query.id_token;
-        this.access_token = query.access_token;
+      it('responds with a nested encrypted and signed id_token JWT', function () {
+        expect(this.id_token).to.be.ok;
+        expect(this.id_token.split('.')).to.have.lengthOf(5);
+
+        return jose.JWE.createDecrypt(this.keystore).decrypt(this.id_token).then((result) => {
+          expect(result.payload).to.be.ok;
+          expect(result.payload.toString().split('.')).to.have.lengthOf(3);
+          expect(JWT.decode(result.payload)).to.be.ok;
+        });
       });
-    });
 
-    it('responds with a nested encrypted and signed id_token JWT', function () {
-      expect(this.id_token).to.be.ok;
-      expect(this.id_token.split('.')).to.have.lengthOf(5);
-
-      return jose.JWE.createDecrypt(this.keystore).decrypt(this.id_token).then((result) => {
-        expect(result.payload).to.be.ok;
-        expect(result.payload.toString().split('.')).to.have.lengthOf(3);
-        expect(JWT.decode(result.payload)).to.be.ok;
-      });
-    });
-
-    it('responds with a nested encrypted and signed userinfo JWT', function (done) {
-      agent.get('/me')
+      it('responds with an encrypted userinfo JWT', function (done) {
+        agent.get('/me')
         .set('Authorization', `Bearer ${this.access_token}`)
         .expect(200)
         .expect('content-type', /application\/jwt/)
@@ -63,21 +65,56 @@ before(function () {
           .decrypt(response.text)
           .then((result) => {
             expect(result.payload).to.be.ok;
-            expect(result.payload.toString().split('.')).to.have.lengthOf(3);
-            const decode = JWT.decode(result.payload);
-            expect(decode).to.be.ok;
-            expect(decode.payload).to.have.property('exp').above(Date.now() / 1000);
+
+            expect(JSON.parse(result.payload)).to.have.keys('sub');
           })
           .then(done, done);
         });
+      });
+
+      describe('userinfo nested signed and encrypted', function () {
+        before(function * () {
+          const client = yield provider.get('Client').find('client');
+          client.userinfoSignedResponseAlg = 'RS256';
+        });
+
+        after(function * () {
+          const client = yield provider.get('Client').find('client');
+          client.userinfoSignedResponseAlg = undefined;
+        });
+
+        it('also handles nested encrypted and signed userinfo JWT', function (done) {
+          agent.get('/me')
+          .set('Authorization', `Bearer ${this.access_token}`)
+          .expect(200)
+          .expect('content-type', /application\/jwt/)
+          .expect((response) => {
+            expect(response.text.split('.')).to.have.lengthOf(5);
+          })
+          .end((err, response) => {
+            if (err) throw err;
+            jose.JWE.createDecrypt(this.keystore)
+            .decrypt(response.text)
+            .then((result) => {
+              expect(result.payload).to.be.ok;
+              expect(result.payload.toString().split('.')).to.have.lengthOf(3);
+              const decode = JWT.decode(result.payload);
+              expect(decode).to.be.ok;
+              expect(decode.payload).to.have.property('exp').above(Date.now() / 1000);
+            })
+            .then(done, done);
+          });
+        });
+      });
     });
 
-    it('works with signed by none', function () {
-      return JWT.sign({
-        client_id: 'client',
-        response_type: 'code',
-        redirect_uri: 'https://client.example.com/cb'
-      }, null, 'none').then((signed) =>
+    describe('authorization request object encryption', function () {
+      it('works with signed by none', function () {
+        return JWT.sign({
+          client_id: 'client',
+          response_type: 'code',
+          redirect_uri: 'https://client.example.com/cb'
+        }, null, 'none').then((signed) =>
         JWT.encrypt(signed, provider.keystore.get(), 'A128CBC-HS256', 'RSA1_5')
       ).then((encrypted) =>
         wrap({
@@ -91,16 +128,17 @@ before(function () {
             response_type: 'code'
           }
         })
-        .expect(302)
-        .expect(function (response) {
-          const expected = parse('https://client.example.com/cb', true);
-          const actual = parse(response.headers.location, true);
-          ['protocol', 'host', 'pathname'].forEach((attr) => {
-            expect(actual[attr]).to.equal(expected[attr]);
-          });
-          expect(actual.query).to.have.property('code');
-        })
-      );
+          .expect(302)
+          .expect(function (response) {
+            const expected = parse('https://client.example.com/cb', true);
+            const actual = parse(response.headers.location, true);
+            ['protocol', 'host', 'pathname'].forEach((attr) => {
+              expect(actual[attr]).to.equal(expected[attr]);
+            });
+            expect(actual.query).to.have.property('code');
+          })
+        );
+      });
     });
 
     it('handles when no suitable encryption key is found', function * () {
