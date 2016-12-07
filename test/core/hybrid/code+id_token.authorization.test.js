14d641048b4d26863df78957378c44c2b68d1590
Filip Skokan <panva.ip@gmail.com>
2016-12-07 18:35:25 +0100

    offline_access should be ignored when consent is not present, invalid_request is wrong

--- test/core/hybrid/code+id_token.authorization.test.js
+++ test/core/hybrid/code+id_token.authorization.test.js
@@ -26,6 +26,24 @@ describe('HYBRID code+id_token', function () {
           .expect(auth.validateState)
           .expect(auth.validateClientLocation);
       });
+
+      it('ignores the scope offline_access unless prompt consent is present', function () {
+        const spy = sinon.spy();
+        this.provider.once('token.issued', spy);
+
+        const auth = new this.AuthorizationRequest({
+          response_type: 'code id_token',
+          scope: 'openid offline_access'
+        });
+
+        return this.wrap({ route, verb, auth })
+          .expect(302)
+          .expect(auth.validateFragment)
+          .expect(auth.validateClientLocation)
+          .expect(() => {
+            expect(spy.firstCall.args[0]).to.have.property('scope').and.not.include('offline_access');
+          });
+      });
     });
 
     describe(`${verb} ${route} errors`, function () {
@@ -48,27 +66,6 @@ describe('HYBRID code+id_token', function () {
           .expect(auth.validateClientLocation)
           .expect(auth.validateError('invalid_request'))
           .expect(auth.validateErrorDescription('response_mode not allowed for this response_type'));
-      });
-
-      it('invalid use of scope offline_access', function () {
-        const spy = sinon.spy();
-        this.provider.once('authorization.error', spy);
-        const auth = new this.AuthorizationRequest({
-          response_type: 'code id_token',
-          scope: 'openid offline_access'
-        });
-
-        return this.wrap({ route, verb, auth })
-        .expect(302)
-        .expect(() => {
-          expect(spy.calledOnce).to.be.true;
-        })
-        .expect(auth.validateFragment)
-        .expect(auth.validatePresence(['error', 'error_description', 'state']))
-        .expect(auth.validateState)
-        .expect(auth.validateClientLocation)
-        .expect(auth.validateError('invalid_request'))
-        .expect(auth.validateErrorDescription('offline_access scope requires consent prompt'));
       });
 
       it('missing mandatory parameter nonce', function () {
