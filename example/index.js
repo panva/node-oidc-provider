a33ebf512f62e65794b07c1cca939fe774172061
Filip Skokan <panva.ip@gmail.com>
2016-12-30 23:00:03 +0100

    removing mounts from example, fixing em too

--- example/index.js
+++ example/index.js
@@ -5,22 +5,12 @@
 const LIB = require('../lib');
 const path = require('path');
 const _ = require('lodash');
-const koa = require('koa');
 const bodyParser = require('koa-body');
 const querystring = require('querystring');
 const Router = require('koa-router');
 const render = require('koa-ejs');
 
 const port = process.env.PORT || 3000;
-const app = koa();
-
-render(app, {
-  cache: false,
-  layout: '_layout',
-  root: path.join(__dirname, 'views'),
-});
-
-app.keys = ['some secret key', 'and also the old one'];
 
 const Account = require('./account');
 const settings = require('./settings');
@@ -29,50 +19,53 @@ const Provider = LIB.Provider;
 
 const issuer = process.env.ISSUER || 'http://localhost:3000';
 
-if (process.env.NODE_ENV === 'production') {
-  app.proxy = true;
-  _.set(settings.config, 'cookies.short.secure', true);
-  _.set(settings.config, 'cookies.long.secure', true);
-
-  app.use(function* ensureSecure(next) {
-    if (this.secure) {
-      yield next;
-    } else if (this.method === 'GET' || this.method === 'HEAD') {
-      this.redirect(this.href.replace(/^http:\/\//i, 'https://'));
-    } else {
-      this.body = {
-        error: 'invalid_request',
-        error_description: 'do yourself a favor and only use https',
-      };
-      this.status = 400;
-    }
-  });
-}
-
 if (process.env.MONGODB_URI) {
   const MongoAdapter = require('./adapters/mongodb'); // eslint-disable-line global-require
-
   settings.config.adapter = MongoAdapter;
 }
 
 settings.config.findById = Account.findById;
+const clients = settings.clients;
 
-Promise.all([
-  LIB.asKeyStore({ keys: settings.certificates }),
-  LIB.asKeyStore({ keys: settings.integrityKeys }),
-]).then((results) => {
-  const keystore = results[0];
-  const integrity = results[1];
-  const clients = settings.clients;
+const provider = new Provider(issuer, settings.config);
 
-  const provider = new Provider(issuer, settings.config);
+if (process.env.HEROKU) {
+  provider.defaultHttpOptions = { timeout: 15000 };
+}
 
-  if (process.env.HEROKU) {
-    provider.defaultHttpOptions = { timeout: 15000 };
+provider.initialize({
+  clients,
+  keystore: { keys: settings.certificates },
+  integrity: { keys: settings.integrityKeys },
+}).then(() => {
+  render(provider.app, {
+    cache: false,
+    layout: '_layout',
+    root: path.join(__dirname, 'views'),
+  });
+
+  provider.app.keys = ['some secret key', 'and also the old one'];
+
+  if (process.env.NODE_ENV === 'production') {
+    provider.app.proxy = true;
+    _.set(settings.config, 'cookies.short.secure', true);
+    _.set(settings.config, 'cookies.long.secure', true);
+
+    provider.app.middleware.unshift(function* ensureSecure(next) {
+      if (this.secure) {
+        yield next;
+      } else if (this.method === 'GET' || this.method === 'HEAD') {
+        this.redirect(this.href.replace(/^http:\/\//i, 'https://'));
+      } else {
+        this.body = {
+          error: 'invalid_request',
+          error_description: 'do yourself a favor and only use https',
+        };
+        this.status = 400;
+      }
+    });
   }
 
-  return provider.initialize({ keystore, integrity, clients });
-}).then((provider) => {
   const router = new Router();
 
   router.get('/interaction/:grant', function* renderInteraction(next) {
@@ -133,9 +126,9 @@ Promise.all([
     provider.resume(this, this.request.body.uuid, result);
   });
 
-  app.use(router.routes());
+  provider.app.use(router.routes());
 })
-.then(() => app.listen(port))
+.then(() => provider.app.listen(port))
 .catch((err) => {
   console.error(err);
   process.exit(1);
