b156dae2fe090e34a2c1d64d7276e9876d4b2b03
Filip Skokan <panva.ip@gmail.com>
2017-04-29 17:20:34 +0200

    example ttls reconfigured

--- example/settings.js
+++ example/settings.js
@@ -3,7 +3,7 @@ const pkg = require('../package.json');
 module.exports.config = {
   acrValues: ['session', 'urn:mace:incommon:iap:bronze'],
   cookies: {
-    long: { signed: true },
+    long: { signed: true, maxAge: (1 * 24 * 60 * 60) * 1000 }, // 1 day in ms
     short: { signed: true },
   },
   discovery: {
@@ -38,6 +38,13 @@ module.exports.config = {
   pairwiseSalt: 'da1c442b365b563dfc121f285a11eedee5bbff7110d55c88',
   interactionUrl: function interactionUrl(ctx, interaction) { // eslint-disable-line no-unused-vars
     return `/interaction/${ctx.oidc.uuid}`;
+  },
+  ttl: {
+    AccessToken: 1 * 60 * 60, // 1 hour in seconds
+    AuthorizationCode: 10 * 60, // 10 minutes in seconds
+    ClientCredentials: 10 * 60, // 10 minutes in seconds
+    IdToken: 1 * 60 * 60, // 1 hour in seconds
+    RefreshToken: 1 * 24 * 60 * 60, // 1 day in seconds
   },
 };
 
