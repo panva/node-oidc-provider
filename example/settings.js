7014bccd3d46631394f6db26900c1a760dcb7db9
Filip Skokan <panva.ip@gmail.com>
2017-05-05 21:45:45 +0200

    return docs

--- example/settings.js
+++ example/settings.js
@@ -36,6 +36,7 @@ module.exports.config = {
     sessionManagement: true,
     backchannelLogout: true,
     oauthNativeApps: true,
+    pkce: { skipClientAuth: true },
   },
   subjectTypes: ['public', 'pairwise'],
   pairwiseSalt: 'da1c442b365b563dfc121f285a11eedee5bbff7110d55c88',
