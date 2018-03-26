40c6935055ad6a1a31267c6d6c6c7c0c72e66c12
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2018-03-26 11:37:13 +0000

    Add tests to check PKCE fine tuning

--- test/pkce/pkce.test.js
+++ test/pkce/pkce.test.js
@@ -127,6 +127,66 @@ describe('PKCE RFC7636', () => {
           .expect(auth.validatePresence(['id_token', 'state']));
       });
     });
+
+    describe('only S256 is supported', () => {
+      before(function () {
+        i(this.provider).configuration('features.pkce').supportedMethods = ['S256'];
+      });
+
+      after(function () {
+        i(this.provider).configuration('features.pkce').supportedMethods = ['plain', 'S256'];
+      });
+
+      it('fails when client does not provide challenge method', function () {
+        const auth = new this.AuthorizationRequest({
+          response_type: 'code',
+          scope: 'openid',
+          code_challenge: 'foobar',
+        });
+
+        return this.agent.get('/auth')
+          .query(auth)
+          .expect(auth.validatePresence(['error', 'error_description', 'state']))
+          .expect(auth.validateError('invalid_request'))
+          .expect(auth.validateErrorDescription('not supported value of code_challenge_method'));
+      });
+
+      it('fails when client uses plain method', function () {
+        const auth = new this.AuthorizationRequest({
+          response_type: 'code',
+          scope: 'openid',
+          code_challenge: 'foobar',
+          code_challenge_method: 'plain',
+        });
+
+        return this.agent.get('/auth')
+          .query(auth)
+          .expect(auth.validatePresence(['error', 'error_description', 'state']))
+          .expect(auth.validateError('invalid_request'))
+          .expect(auth.validateErrorDescription('not supported value of code_challenge_method'));
+      });
+
+      it('stores codeChallenge and codeChallengeMethod in the code', function () {
+        const auth = new this.AuthorizationRequest({
+          response_type: 'code',
+          scope: 'openid',
+          code_challenge: 'E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM',
+          code_challenge_method: 'S256',
+        });
+
+        return this.agent.get('/auth')
+          .query(auth)
+          .expect((response) => {
+            const { query: { code } } = parseUrl(response.headers.location, true);
+            const jti = code.substring(0, 48);
+            const stored = this.TestAdapter.for('AuthorizationCode').syncFind(jti);
+            const payload = JSON.parse(base64url.decode(stored.payload));
+
+            expect(payload).to.have.property('codeChallengeMethod', 'S256');
+            expect(payload).to.have.property('codeChallenge', 'E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM');
+          });
+      });
+    });
   });
 
   describe('token grant_type=authorization_code', () => {
@@ -251,6 +311,66 @@ describe('PKCE RFC7636', () => {
         .expect((response) => {
           expect(response.body).to.have.property('error', 'invalid_grant');
         });
+    });
+
+    describe('only S256 is supported', () => {
+      before(function () {
+        i(this.provider).configuration('features.pkce').supportedMethods = ['S256'];
+      });
+
+      after(function () {
+        i(this.provider).configuration('features.pkce').supportedMethods = ['plain', 'S256'];
+      });
+
+      it('fails if plain is used', async function () {
+        const authCode = new this.provider.AuthorizationCode({
+          accountId: 'sub',
+          scope: 'openid',
+          clientId: 'client',
+          codeChallenge: 'plainFoobar',
+          codeChallengeMethod: 'plain',
+          redirectUri: 'com.example.myapp:/localhost/cb',
+        });
+        const code = await authCode.save();
+
+        return this.agent.post('/token')
+          .auth('client', 'secret')
+          .type('form')
+          .send({
+            code,
+            grant_type: 'authorization_code',
+            redirect_uri: 'com.example.myapp:/localhost/cb',
+            code_verifier: 'plainFoobar',
+          })
+          .expect(400)
+          .expect((response) => {
+            expect(response.body).to.have.property('error', 'invalid_grant');
+            expect(response.body).to.have.property('error_description', 'grant request is invalid');
+          });
+      });
+
+      it('passes if S256 is used', async function () {
+        const authCode = new this.provider.AuthorizationCode({
+          accountId: 'sub',
+          scope: 'openid',
+          clientId: 'client',
+          codeChallenge: 'E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM',
+          codeChallengeMethod: 'S256',
+          redirectUri: 'com.example.myapp:/localhost/cb',
+        });
+        const code = await authCode.save();
+
+        return this.agent.post('/token')
+          .auth('client', 'secret')
+          .type('form')
+          .send({
+            code,
+            grant_type: 'authorization_code',
+            redirect_uri: 'com.example.myapp:/localhost/cb',
+            code_verifier: 'dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk',
+          })
+          .expect(200);
+      });
     });
   });
 });
