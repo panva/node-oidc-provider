ec6ce8081255d16cbc8b5e5b3170dac2dba4a12c
Marc-Aurèle DARCHE <madarche@users.noreply.github.com>
2018-04-14 18:43:57 +0200

    Document #claims method new parameters in example

--- example/account.js
+++ example/account.js
@@ -8,7 +8,15 @@ class Account {
     store.set(this.accountId, this);
   }
 
-  claims() {
+  /**
+   * @param use - can either be "id_token" or "userinfo", depending on
+   *   where the specific claims are intended to be put in.
+   * @param scope - the intended scope, while oidc-provider will mask
+   *   claims depending on the scope automatically you might want to skip
+   *   loading some claims from external resources etc. based on this detail
+   *   or not return them in id tokens but only userinfo and so on.
+   */
+  async claims(use, scope) { // eslint-disable-line no-unused-vars
     return {
       sub: this.accountId, // it is essential to always return a sub claim
 
