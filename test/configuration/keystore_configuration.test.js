387ee6d0415146f69b23ee1a841a9ef8d67f6128
Filip Skokan <panva.ip@gmail.com>
2018-05-17 15:54:09 -0500

    conform RS256 presence check

--- test/configuration/keystore_configuration.test.js
+++ test/configuration/keystore_configuration.test.js
@@ -4,27 +4,47 @@ const jose = require('node-jose');
 const { expect } = require('chai');
 const Provider = require('../../lib');
 
-const keystore = jose.JWK.createKeyStore();
 const fail = () => { throw new Error('expected promise to be rejected'); };
 
 describe('configuration.keystore', () => {
-  it('must contain at least one RS256 signing key', () => {
+  beforeEach(function () {
+    this.keystore = jose.JWK.createKeyStore();
+  });
+
+  it('must contain at least one RS256 signing key', function () {
     const provider = new Provider('http://localhost');
 
-    return provider.initialize({ keystore }).then(fail, (err) => {
+    return provider.initialize({ keystore: this.keystore }).then(fail, (err) => {
       expect(err.message).to.equal('RS256 signing must be supported but no viable key was provided');
-    }).then(() => keystore.generate('RSA', 256)).then(() => {
-      provider.initialize({ keystore });
+    }).then(() => this.keystore.generate('RSA', 256)).then(() => {
+      provider.initialize({ keystore: this.keystore });
     });
   });
 
-  it('must only contain EC and RS keys', () => {
+  it('must only contain EC and RS keys', async function () {
     const provider = new Provider('http://localhost');
+    await this.keystore.generate('RSA', 256);
 
-    return keystore.generate('oct', 256)
-      .then(() => provider.initialize({ keystore }))
+    return this.keystore.generate('oct', 256)
+      .then(() => provider.initialize({ keystore: this.keystore }))
       .then(fail, (err) => {
         expect(err.message).to.equal('only private RSA or EC keys should be part of keystore configuration');
       });
+  });
+
+  it('allows to initialize without RS256 if ID Tokens only come from the token_endpoint', function () {
+    const provider = new Provider('http://localhost', {
+      responseTypes: [
+        'code',
+        'none',
+        'code token',
+        'token',
+      ],
+      unsupported: {
+        idTokenSigningAlgValues: ['HS256', 'HS384', 'HS512'],
+      },
+    });
+
+    return provider.initialize({ keystore: this.keystore });
   });
 });
