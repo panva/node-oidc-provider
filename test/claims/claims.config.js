85c4c6920eb9c1360059a6659d97b027751ecdd0
Filip Skokan <panva.ip@gmail.com>
2018-09-09 21:46:41 +0200

    feat: allow custom mechanisms for handling pairwise identifiers
    BREAKING CHANGE: the configuration option `pairwiseSalt` is replaced
    with `pairwiseIdentifier` async helper function. This allows for
    different means of generating the pairwise identifier to be implemented,
    such as the ones mentioned in [Core 1.0](https://openid.net/specs/openid-connect-core-1_0.html#rfc.section.8.1)

--- test/claims/claims.config.js
+++ test/claims/claims.config.js
@@ -2,16 +2,28 @@ const { clone } = require('lodash');
 
 const config = clone(require('../default.config'));
 
+config.subjectTypes = ['pairwise', 'public'];
 config.features = { claimsParameter: true };
 config.acrValues = ['0', '1', '2'];
+config.pairwiseIdentifier = sub => `${sub}-pairwise`;
 
 module.exports = {
   config,
-  client: {
-    client_id: 'client',
-    client_secret: 'secret',
-    grant_types: ['implicit'],
-    response_types: ['id_token token', 'none', 'id_token'],
-    redirect_uris: ['https://client.example.com/cb'],
-  },
+  clients: [
+    {
+      client_id: 'client',
+      client_secret: 'secret',
+      grant_types: ['implicit'],
+      response_types: ['id_token token', 'none', 'id_token'],
+      redirect_uris: ['https://client.example.com/cb'],
+    },
+    {
+      client_id: 'client-pairwise',
+      subject_type: 'pairwise',
+      token_endpoint_auth_method: 'none',
+      grant_types: ['implicit'],
+      response_types: ['id_token'],
+      redirect_uris: ['https://client.example.com/cb'],
+    },
+  ],
 };
