ab08cbef610dce76249f7ff893d2b1089a15f07d
Filip Skokan <panva.ip@gmail.com>
2019-04-03 20:45:17 +0200

    fix: session required client properties control the iss & sid return

--- test/backchannel_logout/backchannel_logout.test.js
+++ test/backchannel_logout/backchannel_logout.test.js
@@ -24,15 +24,35 @@ describe('Back-Channel Logout 1.0', () => {
         .filteringRequestBody((body) => {
           expect(body).to.match(/^logout_token=(([\w-]+\.?){3})$/);
           const decoded = JSON.parse(base64url.decode(RegExp.$1.split('.')[1]));
-          expect(decoded).to.have.all.keys('sub', 'events', 'iat', 'aud', 'iss', 'jti');
+          expect(decoded).to.have.all.keys('sub', 'events', 'iat', 'aud', 'iss', 'jti', 'sid');
           expect(decoded).to.have.property('events').and.eql({ 'http://schemas.openid.net/event/backchannel-logout': {} });
           expect(decoded).to.have.property('aud', 'client');
           expect(decoded).to.have.property('sub', 'subject');
+          expect(decoded).to.have.property('sid', 'foo');
         })
         .post('/backchannel_logout')
         .reply(204);
 
-      return client.backchannelLogout('subject');
+      return client.backchannelLogout('subject', 'foo');
+    });
+
+    it('omits sid when its not required', async function () {
+      const client = await this.provider.Client.find('no-sid');
+
+      nock('https://no-sid.example.com/')
+        .filteringRequestBody((body) => {
+          expect(body).to.match(/^logout_token=(([\w-]+\.?){3})$/);
+          const decoded = JSON.parse(base64url.decode(RegExp.$1.split('.')[1]));
+          expect(decoded).to.have.all.keys('sub', 'events', 'iat', 'aud', 'iss', 'jti');
+          expect(decoded).to.have.property('events').and.eql({ 'http://schemas.openid.net/event/backchannel-logout': {} });
+          expect(decoded).to.have.property('aud', 'no-sid');
+          expect(decoded).to.have.property('sub', 'subject');
+          expect(decoded).not.to.have.property('sid');
+        })
+        .post('/backchannel_logout')
+        .reply(204);
+
+      return client.backchannelLogout('subject', 'foo');
     });
   });
 
