9a786653aa6250453c0ed69c06133ce8def67777
big-kahuna-burger <arandjel.sarenac@hotmail.co.uk>
2019-04-05 21:46:00 +0200

    test: address reviewer's comments

--- test/helpers/initialize_adapter.test.js
+++ test/helpers/initialize_adapter.test.js
@@ -5,7 +5,7 @@ const initializeAdapter = require('../../lib/helpers/initialize_adapter');
 describe('initializeAdapter helper', () => {
   it('throws when adapter is not a constructor', () => {
     initializeAdapter(() => {}).catch(err => expect(err.message).to.be.equal(
-      "Expected 'adapter' to be a constructor, provide a valid adapter in Provider config.",
+      'Expected "adapter" to be a constructor, provide a valid adapter in Provider config.',
     ));
   });
 });
