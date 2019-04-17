71d61bd23b6fbc6126dab9225432b9d1f60d82f4
Filip Skokan <panva.ip@gmail.com>
2019-04-17 22:05:46 +0200

    refactor: cors behaviours refactored

---
+++ test/cors/cors.config.js
@@ -0,0 +1,21 @@
+const { clone } = require('lodash');
+
+const config = clone(require('../default.config'));
+
+config.features = {
+  introspection: { enabled: true },
+  revocation: { enabled: true },
+  deviceFlow: { enabled: true },
+  clientCredentials: { enabled: true },
+};
+
+module.exports = {
+  config,
+  client: {
+    client_id: 'client',
+    grant_types: ['client_credentials', 'urn:ietf:params:oauth:grant-type:device_code'],
+    response_types: [],
+    redirect_uris: [],
+    token_endpoint_auth_method: 'none',
+  },
+};
