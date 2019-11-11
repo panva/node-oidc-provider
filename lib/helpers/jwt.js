panva/node-oidc-provider

commit 58f73480c92c4180a4616ed1e698b7925e4a07d3

refactor: skip redundant JWT payload parsing

lib/helpers/jwt.js
@@ -131,18 +131,27 @@ class JWT {
   }
 
   static async verify(jwt, keyOrStore, options = {}) {
-    const { payload, header } = JWT.decode(jwt);
-    JWT.assertHeader(header, options);
+    const opts = {
+      complete: true,
+      parse: false,
+      algorithms: options.algorithm ? [options.algorithm] : undefined,
+    };
+
+    let payload;
+    let header;
+
     try {
-      JWS.verify(jwt, keyOrStore, { complete: true });
+      ({ protected: header, payload } = JWS.verify(jwt, keyOrStore, opts));
     } catch (err) {
-      if (isKeyStore(keyOrStore)) {
+      if (isKeyStore(keyOrStore) && keyOrStore.stale()) {
         await keyOrStore.refresh();
-        JWS.verify(jwt, keyOrStore, { complete: true });
+        ({ protected: header, payload } = JWS.verify(jwt, keyOrStore, opts));
       } else {
         throw err;
       }
     }
+
+    payload = JSON.parse(payload);
 
     this.assertPayload(payload, options);
     return { payload, header };

commit 4738a8b68c2cbc6c15a1352d2c22ebba7bda839a

fix: regression introduced in 58f7348

lib/helpers/jwt.js
@@ -143,12 +143,12 @@ class JWT {
     try {
       ({ protected: header, payload } = JWS.verify(jwt, keyOrStore, opts));
     } catch (err) {
-      if (isKeyStore(keyOrStore) && keyOrStore.stale()) {
-        await keyOrStore.refresh();
-        ({ protected: header, payload } = JWS.verify(jwt, keyOrStore, opts));
-      } else {
+      if (!isKeyStore(keyOrStore) || typeof keyOrStore.fresh !== 'function' || keyOrStore.fresh()) {
         throw err;
       }
+
+      await keyOrStore.refresh();
+      ({ protected: header, payload } = JWS.verify(jwt, keyOrStore, opts));
     }
 
     payload = JSON.parse(payload);