efe4e21c34118a9ce13ebb8ea3cd84a178c0134c
Filip Skokan <panva.ip@gmail.com>
2019-11-28 12:37:28 +0100

    chore: warn when provider keystore `kid` is not unique

--- test/configuration/keystore_configuration.test.js
+++ test/configuration/keystore_configuration.test.js
@@ -1,6 +1,7 @@
-/* eslint-disable no-new */
+/* eslint-disable no-new, no-console */
 
 const jose = require('jose');
+const sinon = require('sinon').createSandbox();
 const { expect } = require('chai');
 
 const { Provider } = require('../../lib');
@@ -10,6 +11,8 @@ describe('configuration.jwks', () => {
   beforeEach(function () {
     this.keystore = new jose.JWKS.KeyStore();
   });
+
+  afterEach(sinon.restore);
 
   it('must be a valid JWKS object', async function () {
     expect(() => {
@@ -37,6 +40,19 @@ describe('configuration.jwks', () => {
     expect(() => {
       new Provider('http://localhost', { jwks: { keys: [this.keystore.get().toJWK()] } });
     }).to.throw('only private RSA, EC or OKP keys should be part of keystore configuration');
+  });
+
+  it('warns if "kid" is the same for multiple keys', async () => {
+    const ks = new jose.JWKS.KeyStore();
+    sinon.stub(console, 'warn').returns();
+    await Promise.all([
+      ks.generate('RSA', undefined, { kid: 'nov-2019' }),
+      ks.generate('EC', undefined, { kid: 'nov-2019' }),
+    ]);
+    new Provider('http://localhost', {
+      jwks: ks.toJWKS(true),
+    });
+    expect(console.warn.calledWithMatch(/different keys within the keystore SHOULD use distinct `kid` values, with your current keystore you should expect interoperability issues with your clients/)).to.be.true;
   });
 
   it('allows to instantiate without RS256 if ID Tokens only come from the token_endpoint', function () {
