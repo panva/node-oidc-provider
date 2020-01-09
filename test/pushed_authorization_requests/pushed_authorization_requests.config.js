05092c7bdc460e6783acb2e5c289e06aed3c86ba
Filip Skokan <panva.ip@gmail.com>
2020-01-09 10:17:54 +0100

    test: make sure PAR works even without none enabled

--- test/pushed_authorization_requests/pushed_authorization_requests.config.js
+++ test/pushed_authorization_requests/pushed_authorization_requests.config.js
@@ -3,6 +3,8 @@ const merge = require('lodash/merge');
 
 const config = cloneDeep(require('../default.config'));
 
+config.whitelistedJWA.requestObjectSigningAlgValues = config.whitelistedJWA.requestObjectSigningAlgValues.filter((alg) => alg !== 'none');
+
 merge(config.features, {
   pushedAuthorizationRequests: { enabled: true },
 });
