6e6ad9231b9a92b1edf8b3838a4e293c5ba0ee37
Filip Skokan <panva.ip@gmail.com>
2020-06-30 17:33:04 +0200

    test: fix not running the right formats

--- test/base_token/base_token.config.js
+++ test/base_token/base_token.config.js
@@ -2,12 +2,6 @@ const cloneDeep = require('lodash/cloneDeep');
 
 const config = cloneDeep(require('../default.config'));
 
-const { formats: { AccessToken: FORMAT } } = global.TEST_CONFIGURATION_DEFAULTS;
-
-config.formats = {
-  default: FORMAT,
-};
-
 module.exports = {
   config,
 };
