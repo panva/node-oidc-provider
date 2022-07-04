de7af4926d312f994fbe0e7b3921c7f2e225cc4d
Filip Skokan <panva.ip@gmail.com>
2022-07-04 11:47:14 +0200

    add tests

--- test/helpers/initialize_adapter.test.js
+++ test/helpers/initialize_adapter.test.js
@@ -4,8 +4,8 @@ const initializeAdapter = require('../../lib/helpers/initialize_adapter');
 
 describe('initializeAdapter helper', () => {
   it('throws when adapter is not a constructor or not a function', () => {
-    expect(initializeAdapter.bind(undefined, {})).to.throw('Expected "adapter" to be a constructor or not async function, provide a valid adapter in Provider config.');
-    expect(initializeAdapter.bind(undefined, async () => {})).to.throw('Expected "adapter" to be a constructor or not async function, provide a valid adapter in Provider config.');
+    expect(initializeAdapter.bind(undefined, {})).to.throw('Expected "adapter" to be a constructor or a factory function, provide a valid adapter in Provider config.');
+    expect(initializeAdapter.bind(undefined, async () => {})).to.throw('Expected "adapter" to be a constructor or a factory function, provide a valid adapter in Provider config.');
   });
 
   it('should be success if argument is static method of class', () => {
