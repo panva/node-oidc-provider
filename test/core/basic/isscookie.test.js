6effeed4629e1dc229e00d93cdec45b8cf4d015e
Filip Skokan <panva.ip@gmail.com>
2024-11-05 09:36:19 +0000

    fix: normalize single string set-cookie headers

---
+++ test/core/basic/isscookie.test.js
@@ -0,0 +1,23 @@
+import bootstrap from '../../test_helper.js';
+
+describe('pre-middleware setting "set-cookie" header', () => {
+  before(bootstrap(import.meta.url));
+
+  before(function () {
+    this.provider.use((ctx, next) => {
+      ctx.response.set('set-cookie', 'foo=bar;');
+      return next();
+    });
+  });
+
+  it('does not disturb the session middleware', function () {
+    const auth = new this.AuthorizationRequest({
+      response_type: 'invalid',
+      state: null,
+    });
+
+    return this.wrap({ route: '/auth', verb: 'get', auth })
+      .expect(303)
+      .expect(auth.validatePresence(['error', 'error_description']));
+  });
+});
