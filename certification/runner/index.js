d93fedbd2f52c74c50447d16491b37779286237d
Filip Skokan <panva.ip@gmail.com>
2025-04-12 07:55:06 +0200

    test: update conformance suite plans

--- certification/runner/index.js
+++ certification/runner/index.js
@@ -47,36 +47,20 @@ switch (PLAN_NAME) {
     VARIANT.response_type = 'code';
     break;
   case 'oidcc-basic-certification-test-plan':
-    // TODO: unskip when https://gitlab.com/openid/conformance-suite/-/issues/1497 is fixed
-    SKIP = 'oidcc-request-uri-unsigned-supported-correctly-or-rejected-as-unsupported';
     VARIANT.server_metadata = 'discovery';
     VARIANT.client_registration = 'dynamic_client';
     break;
   case 'oidcc-hybrid-certification-test-plan':
-    // TODO: unskip when https://gitlab.com/openid/conformance-suite/-/issues/1497 is fixed
-    SKIP = 'oidcc-request-uri-unsigned-supported-correctly-or-rejected-as-unsupported';
     VARIANT.server_metadata = 'discovery';
     VARIANT.client_registration = 'dynamic_client';
     break;
   case 'oidcc-implicit-certification-test-plan':
-    // TODO: unskip when https://gitlab.com/openid/conformance-suite/-/issues/1497 is fixed
-    SKIP = 'oidcc-request-uri-unsigned-supported-correctly-or-rejected-as-unsupported';
     VARIANT.server_metadata = 'discovery';
     VARIANT.client_registration = 'dynamic_client';
     break;
-  case 'fapi2-message-signing-id1-test-plan':
-    if (VARIANT.client_auth_type === 'private_key_jwt') {
-      // TODO: unskip when https://gitlab.com/openid/conformance-suite/-/issues/1443#note_2430893342 is fixed
-      SKIP = 'fapi2-security-profile-id2-par-test-pushed-authorization-url-as-audience-for-client-JWT-assertion,fapi2-security-profile-id2-par-test-token-endpoint-url-as-audience-for-client-JWT-assertion,fapi2-security-profile-id2-test-array-as-audience-for-client-JWT-assertion,fapi2-security-profile-id2-attempt-reuse-authorization-code-after-one-second';
-    }
+  case 'fapi2-message-signing-final-test-plan':
     VARIANT.fapi_request_method = 'signed_non_repudiation';
     VARIANT.fapi_response_mode = 'jarm';
-    break;
-  case 'fapi2-security-profile-id2-test-plan':
-    if (VARIANT.client_auth_type === 'private_key_jwt') {
-      // TODO: unskip when https://gitlab.com/openid/conformance-suite/-/issues/1443#note_2430893342 is fixed
-      SKIP = 'fapi2-security-profile-id2-par-test-pushed-authorization-url-as-audience-for-client-JWT-assertion,fapi2-security-profile-id2-par-test-token-endpoint-url-as-audience-for-client-JWT-assertion,fapi2-security-profile-id2-test-array-as-audience-for-client-JWT-assertion,fapi2-security-profile-id2-attempt-reuse-authorization-code-after-one-second';
-    }
     break;
   default:
     break;
@@ -109,9 +93,9 @@ switch (PLAN_NAME) {
     configuration.client2.client_id = `1.0-${revision}-${auth}-two`;
     break;
   }
-  case 'fapi2-security-profile-id2-test-plan':
-  case 'fapi2-message-signing-id1-test-plan': {
-    override = 'fapi2-security-profile-id2';
+  case 'fapi2-security-profile-final-test-plan':
+  case 'fapi2-message-signing-final-test-plan': {
+    override = 'fapi2-security-profile-final';
     const spec = PLAN_NAME.split('-').slice(1, 3).join('').replace('-', ''); // securityprofile or messagesigning
     if (VARIANT.openid === 'plain_oauth') {
       removeOpenidScope(configuration);
