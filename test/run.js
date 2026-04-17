568bea6c76163fc5cae0c738af9e9e1b37266783
Filip Skokan <panva.ip@gmail.com>
2026-04-18 00:05:22 +0200

    chore: enable biome lint/suspicious/useIterableCallbackReturn

--- test/run.js
+++ test/run.js
@@ -55,9 +55,9 @@ async function run() {
 
 try {
   await run();
-  passed.forEach((pass) => console.log('\x1b[32m%s\x1b[0m', pass));
+  passed.forEach((pass) => { console.log('\x1b[32m%s\x1b[0m', pass); });
 } catch (error) {
-  passed.forEach((pass) => console.log('\x1b[32m%s\x1b[0m', pass));
+  passed.forEach((pass) => { console.log('\x1b[32m%s\x1b[0m', pass); });
   if (error instanceof SuiteFailedError) {
     console.log('\x1b[31m%s\x1b[0m', error.message);
   } else {
