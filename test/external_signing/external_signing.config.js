78474ace5fb25b691433451d33cd088efa4e2f54
Filip Skokan <panva.ip@gmail.com>
2026-08-06 19:30:25 +0200

    test: add coverage for async keyObject ExternalSigningKey
    Signed-off-by: Filip Skokan <panva.ip@gmail.com>

--- test/external_signing/external_signing.config.js
+++ test/external_signing/external_signing.config.js
@@ -6,6 +6,7 @@ import { ExternalSigningKey } from '../../lib/index.js';
 import getConfig from '../default.config.js';
 
 const es256 = crypto.generateKeyPairSync('ec', { namedCurve: 'P-256' });
+const es384 = crypto.generateKeyPairSync('ec', { namedCurve: 'P-384' });
 const rs256 = crypto.generateKeyPairSync('rsa', { modulusLength: 2048 });
 
 const config = getConfig();
@@ -28,12 +29,64 @@ class ES256 extends ExternalSigningKey {
   }
 }
 
+class AsyncES384 extends ExternalSigningKey {
+  constructor(kp) {
+    super();
+    this.kp = kp;
+    this.jwk = kp.publicKey.export({ format: 'jwk' });
+    this.keyObjectCalls = 0;
+  }
+
+  get kty() {
+    return this.jwk.kty;
+  }
+
+  get crv() {
+    return this.jwk.crv;
+  }
+
+  get e() {
+    return this.jwk.e;
+  }
+
+  get n() {
+    return this.jwk.n;
+  }
+
+  get x() {
+    return this.jwk.x;
+  }
+
+  get y() {
+    return this.jwk.y;
+  }
+
+  get pub() {
+    return this.jwk.pub;
+  }
+
+  async keyObject() {
+    this.keyObjectCalls++;
+    return this.kp.publicKey;
+  }
+
+  async sign(data) {
+    return crypto.sign('sha384', data, {
+      key: this.kp.privateKey,
+      dsaEncoding: 'ieee-p1363',
+    });
+  }
+}
+
+export const asyncExternalKey = new AsyncES384(es384);
+
 merge(config.features, { externalSigningSupport: { enabled: true } });
 merge(config, {
   jwks: {
     keys: [
       rs256.privateKey.export({ format: 'jwk' }),
       new ES256(es256),
+      asyncExternalKey,
     ],
   },
 });
@@ -56,6 +109,14 @@ export default {
       response_types: ['id_token'],
       redirect_uris: ['https://client.example.com/cb'],
       id_token_signed_response_alg: 'RS256',
+    },
+    {
+      client_id: 'client-sig-external-async',
+      token_endpoint_auth_method: 'none',
+      grant_types: ['implicit'],
+      response_types: ['id_token'],
+      redirect_uris: ['https://client.example.com/cb'],
+      id_token_signed_response_alg: 'ES384',
     },
   ],
 };
--- test/external_signing/external_signing.test.js
+++ test/external_signing/external_signing.test.js
@@ -1,8 +1,11 @@
 import * as url from 'node:url';
 
 import * as jose from 'jose';
+import { expect } from 'chai';
 
 import bootstrap, { enableNetConnect, resetNetConnect } from '../test_helper.js';
+
+import { asyncExternalKey } from './external_signing.config.js';
 
 const route = '/auth';
 const response_type = 'id_token';
@@ -27,6 +30,17 @@ describe('External Signing Keys', () => {
       .expect(auth.validatePresence(['id_token', 'state']))
       .expect(auth.validateState)
       .expect(auth.validateClientLocation);
+  });
+
+  it('publishes external signing metadata through discovery', function () {
+    return this.agent.get('/.well-known/openid-configuration')
+      .expect('Content-Type', /application\/json/)
+      .expect(200)
+      .expect(({ body }) => {
+        expect(body.id_token_signing_alg_values_supported).to.include('ES384');
+        expect(body.jwks_uri).to.equal(this.provider.issuer + this.suitePath('/jwks'));
+        expect(asyncExternalKey.keyObjectCalls).to.equal(0);
+      });
   });
 
   it('but signs with external keys too and verifies them local', async function () {
@@ -59,5 +73,35 @@ describe('External Signing Keys', () => {
       .expect((response) => {
         ({ query: { id_token } } = url.parse(response.headers.location.replace('#', '?'), true));
       });
+  });
+
+  it('awaits an asynchronous keyObject when verifying locally', async function () {
+    const auth = new this.AuthorizationRequest({
+      client_id: 'client-sig-external-async',
+      response_type,
+      scope,
+    });
+
+    let id_token;
+    await this.wrap({ route, verb: 'get', auth })
+      .expect(303)
+      .expect((response) => {
+        ({ query: { id_token } } = url.parse(response.headers.location.replace('#', '?'), true));
+      });
+
+    const calls = asyncExternalKey.keyObjectCalls;
+
+    auth.id_token_hint = id_token;
+    await this.wrap({ route, verb: 'get', auth })
+      .expect(303)
+      .expect(auth.validateFragment)
+      .expect(auth.validatePresence(['id_token', 'state']))
+      .expect(auth.validateState)
+      .expect(auth.validateClientLocation)
+      .expect((response) => {
+        ({ query: { id_token } } = url.parse(response.headers.location.replace('#', '?'), true));
+      });
+
+    expect(asyncExternalKey.keyObjectCalls).to.equal(calls + 1);
   });
 });
